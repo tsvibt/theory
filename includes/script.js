e7427d23022bcc29af7391826f77133f7f41a50b
tsvibt <tsvibt@gmail.com>
2024-07-27 16:59:59 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,6 @@
-let rootURL = window.location.origin + '/' + window.location.pathname.split('/')[1] + '/';
+let rootURL = window.location.origin + '/' + window.location.pathname.split('/theory/')[0] + '/theory/';
 let link = document.createElement('a');
-link.href = rootURL;
+link.href = rootURL + 'index.html';
 link.textContent = 'index';
 document.body.insertAdjacentElement('afterbegin', link);
-
 
