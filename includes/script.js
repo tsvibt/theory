eb02ba0a002dafbf68c6ea4abe9f44a3b0b0c7ca
tsvibt <tsvibt@gmail.com>
2024-07-27 16:39:18 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -1,9 +1,7 @@
+let rootURL = window.location.origin + window.location.pathname.split('/')[1] + '/';
+let link = document.createElement('a');
+link.href = rootURL;
+link.textContent = 'index';
+document.body.insertAdjacentElement('afterbegin', link);
 
-//let newElement = document.createElement('div');
-//newElement.textContent = 'This is a new element';
-//
-let newElementHTML = '<a href="../">home</a>'
 
-//document.body.insertBefore(newElement, document.body.firstChild);
-
-document.body.innerHTML = newElementHTML + document.body.innerHTML;
