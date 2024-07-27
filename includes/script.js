9d8f73bfe12fa4a102f8b064189d23f590c0be58
tsvibt <tsvibt@gmail.com>
2024-07-27 16:32:18 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -1,12 +1,9 @@
 
 //let newElement = document.createElement('div');
 //newElement.textContent = 'This is a new element';
-
-// Insert the new element at the beginning of the body
 //
-let newElementHTML = '<a href="/">home</a>'
+let newElementHTML = '<a href="../">home</a>'
 
 //document.body.insertBefore(newElement, document.body.firstChild);
 
-// Insert the HTML at the beginning of the body
 document.body.innerHTML = newElementHTML + document.body.innerHTML;
