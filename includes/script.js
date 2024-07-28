e34a0c61bb58f23cadf679efdd94bd504c8f3c73
tsvibt <tsvibt@gmail.com>
2024-07-27 17:30:42 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -5,7 +5,7 @@ document.body.innerHTML = newElementHTML + document.body.innerHTML;
 
 
 
-let rootURL = window.location.origin + '/' + window.location.pathname.split('/theory/')[0] + '/theory/';
+let rootURL = window.location.origin + window.location.pathname.split('/theory/')[0] + '/theory/';
 let link = document.createElement('a');
 link.href = rootURL + 'index.html';
 link.textContent = 'index';
