1e664087dd2cf899f8f0c1fcc2dbb84896b81be4
tsvibt <tsvibt@gmail.com>
2024-07-27 17:26:23 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -1,6 +1,17 @@
+
+
+let newElementHTML = '<div class="display-title">' + document.title + '</div>'
+document.body.innerHTML = newElementHTML + document.body.innerHTML;
+
+
+
 let rootURL = window.location.origin + '/' + window.location.pathname.split('/theory/')[0] + '/theory/';
 let link = document.createElement('a');
 link.href = rootURL + 'index.html';
 link.textContent = 'index';
 document.body.insertAdjacentElement('afterbegin', link);
+
+
+
+
 
