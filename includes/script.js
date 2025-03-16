326e7d31882afe11cfd2af95d6f0e0472d86feca
tsvibt <tsvibt@gmail.com>
2025-03-15 19:45:40 -0700

    unnamed

--- includes/script.js
+++ includes/script.js
@@ -10,6 +10,14 @@ let linkHTML = `<a class="top-bar" href="${rootURL}index.html">index</a>`
 document.body.innerHTML = linkHTML + document.body.innerHTML;
 
 
+window.MathJax = {
+  tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] }
+};
+
+var script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+script.async = true;
+document.head.appendChild(script);
 
 
 
